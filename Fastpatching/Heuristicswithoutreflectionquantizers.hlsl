73f5978047478d38fddcc10e75207b29a7d8bf6f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-06 11:07:41 +0200

    Update Heuristicswithoutreflectionquantizers.hlsl

--- Fastpatching/Heuristicswithoutreflectionquantizers.hlsl
+++ Fastpatching/Heuristicswithoutreflectionquantizers.hlsl
@@ -3,3 +3,10 @@
 //like transmission factor and transmission model.
 //as this function below is a' heuristic, we can send it to the cpu
 //but we need a loop.
+
+
+
+//we have access to brandtl model. then, we can 
+//ude latent variables with 10^-3 precision.
+//(mpi prepa). it means it is best to use thenfunction
+//below for diffuse reflection and cook torance brdf.
