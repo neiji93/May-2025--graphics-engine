9d2daaad89c4b2c14c3de746da7c023dad8e86bf
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-06 10:24:25 +0200

    Update Heuristicswithoutreflectionquantizers.hlsl

--- Fastpatching/Heuristicswithoutreflectionquantizers.hlsl
+++ Fastpatching/Heuristicswithoutreflectionquantizers.hlsl
@@ -2,3 +2,4 @@
 //we can quantizize two parametrre in sale time
 //like transmission factor and transmission model.
 //as this function below is a' heuristic, we can send it to the cpu
+//but we need a loop.
